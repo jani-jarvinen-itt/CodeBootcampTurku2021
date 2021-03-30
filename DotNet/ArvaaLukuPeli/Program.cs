8379d0c1dcc1fc8939b22f04ac16c4d524920c87
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-03-30 15:03:51 +0300

    Viimeistelty ja kommentoitu koodia.

--- DotNet/ArvaaLukuPeli/Program.cs
+++ DotNet/ArvaaLukuPeli/Program.cs
@@ -1,23 +1,31 @@
-﻿using System;
+﻿// Arvaa luku -peli. Versio 1.0/30.3.2021. Tekijä: N.N.
+// Copyright (C) Yritys Oy, 2021.
+using System;
 
 namespace ArvaaLukuPeli
 {
     class Program
     {
+        const int SuurinLuku = 20;
+        const int ArvauskertojenMäärä = 3;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Arvaa luku -peli alkaa!");
 
+            // kone arpoo pelin oikean luvun
             Random rand = new Random();
-            int oikea = rand.Next(1, 21);
+            int oikea = rand.Next(1, SuurinLuku + 1);
             // Console.WriteLine(oikea);
 
-            for (int arvauskerta = 1; arvauskerta <= 3; arvauskerta++)
+            // aloitetaan käyttäjän arvausten kysyminen
+            for (int arvauskerta = 1; arvauskerta <= ArvauskertojenMäärä; arvauskerta++)
             {
-                Console.WriteLine("Anna arvauksesi välillä 1-20:");
+                Console.WriteLine($"Anna arvauksesi välillä 1-{SuurinLuku}:");
                 string syöte = Console.ReadLine();
                 int arvaus = int.Parse(syöte);
 
+                // tarkistetaan, oliko vastaus oikea
                 if (arvaus < oikea)
                 {
                     Console.WriteLine("Oikea luku on suurempi!");
