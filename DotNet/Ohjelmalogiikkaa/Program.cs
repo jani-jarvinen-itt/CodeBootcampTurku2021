c30b150a3c95109664d61301393b04f1cb9577ab
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-03-30 10:26:19 +0300

    Aloitettu C#-ohjelmalogiikkademon kehittäminen.

---
+++ DotNet/Ohjelmalogiikkaa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ohjelmalogiikkaa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
