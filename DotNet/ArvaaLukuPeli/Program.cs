62fa0d0e6559d4f8f80275fdebe90bee8c519a18
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-03-30 14:55:02 +0300

    Lisätty kolme arvauskertaa sekä poistettu oikean luvun tulostus.

--- DotNet/ArvaaLukuPeli/Program.cs
+++ DotNet/ArvaaLukuPeli/Program.cs
@@ -10,20 +10,27 @@ namespace ArvaaLukuPeli
 
             Random rand = new Random();
             int oikea = rand.Next(1, 21);
-            Console.WriteLine(oikea);
+            // Console.WriteLine(oikea);
 
-            Console.WriteLine("Anna arvauksesi välillä 1-20:");
-            string syöte = Console.ReadLine();
-            int arvaus = int.Parse(syöte);
+            for (int arvauskerta = 1; arvauskerta <= 3; arvauskerta++)
+            {
+                Console.WriteLine("Anna arvauksesi välillä 1-20:");
+                string syöte = Console.ReadLine();
+                int arvaus = int.Parse(syöte);
 
-            if (arvaus < oikea) {
-                Console.WriteLine("Oikea luku on suurempi!");
-            }
-            else if (arvaus > oikea) {
-                Console.WriteLine("Oikea luku on pienempi!");
-            }
-            else {
-                Console.WriteLine("Jee! Arvasit oikein! Voitit pelin!");
+                if (arvaus < oikea)
+                {
+                    Console.WriteLine("Oikea luku on suurempi!");
+                }
+                else if (arvaus > oikea)
+                {
+                    Console.WriteLine("Oikea luku on pienempi!");
+                }
+                else
+                {
+                    Console.WriteLine("Jee! Arvasit oikein! Voitit pelin!");
+                    break;
+                }
             }
 
             Console.WriteLine("Peli on päättynyt.");
