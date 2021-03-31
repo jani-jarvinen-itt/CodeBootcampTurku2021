bf9f22271595998ffb08f1ca684d92321135e074
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-03-31 13:15:41 +0300

    Muutettu olioiden alustuskoodia modernimmaksi.

--- DotNet/OlioOhjelmointi2/OlioOhjelmointi2/Program.cs
+++ DotNet/OlioOhjelmointi2/OlioOhjelmointi2/Program.cs
@@ -8,12 +8,16 @@ namespace OlioOhjelmointi2
         {
             Console.WriteLine("Moikka Visual Studio!");
 
-            Asiakas asiakas = new Asiakas();
-            asiakas.Nimi = "Teppo Testaaja";
-            asiakas.Osoite = "Kotikatu 12 B";
-            asiakas.Puhelin = "0500 111 222";
-            asiakas.Sähköposti = "teppo@testaajat.fi";
+            // C# 9.0 (2020) alkaen:
+            Asiakas asiakas = new()
+            {
+                Nimi = "Teppo Testaaja",
+                Osoite = "Kotikatu 12 B",
+                Puhelin = "0500 111 222",
+                Sähköposti = "teppo@testaajat.fi"
+            };
 
+            // C# 1.0 (2002) alkaen:
             Tilaus tilaus = new Tilaus();
             tilaus.Päivämäärä = DateTime.Today;
             tilaus.Tuote = "Peruna";
