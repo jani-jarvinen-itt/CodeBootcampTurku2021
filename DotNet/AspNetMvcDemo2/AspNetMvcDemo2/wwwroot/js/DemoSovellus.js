8d41f0887f41d7fe0b051719b58fe3d31eab4690
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-04-01 13:38:27 +0300

    Siirretty laskin-toteutuksen JavaScript-koodi omaan tiedostoonsa.

---
+++ DotNet/AspNetMvcDemo2/AspNetMvcDemo2/wwwroot/js/DemoSovellus.js
@@ -0,0 +1,11 @@
+﻿console.log("Skriptin suoritus alkaa.");
+$("#laskeNappi").click(function () {
+
+    console.log("Nappia klikattu.");
+
+    const luku1 = $("#luku1").val();
+    const luku2 = $("#luku2").val();
+
+    const summa = parseInt(luku1) + parseInt(luku2);
+    alert("Summa on: " + summa);
+});
