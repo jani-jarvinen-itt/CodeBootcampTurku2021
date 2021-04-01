685312c96e73fe653f96991085d8b3252cd44637
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-04-01 10:27:42 +0300

    Aloitettu ASP.NET MVC -sovellusten koodaus.

---
+++ DotNet/AspNetMvcDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetMvcDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
