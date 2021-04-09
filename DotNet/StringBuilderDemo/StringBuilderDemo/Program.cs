c0c24eb5aca3c65f738c8f8500fd9e4f6685de8a
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-04-09 09:47:37 +0300

    Lisätty StringBuilder-esimerkki käyttäen perinteistä merkkijonokäsittelyä silmukassa. Suoritusaika nyt n. 90 sek.

---
+++ DotNet/StringBuilderDemo/StringBuilderDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StringBuilderDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            DateTime alku = DateTime.Now;
+            string html = "<table>";
+            for (int i = 0; i < 100000; i++)
+            {
+                html += "<tr><td>" + i + "</td></tr>\r\n";
+            }
+            html += "</table>";
+            Console.WriteLine("HTML-koodin pituus: " + html.Length);
+            DateTime loppu = DateTime.Now;
+
+            TimeSpan kesto = loppu - alku;
+            Console.WriteLine($"HTML-koodin muodostus kesti: {kesto.TotalSeconds}.");
+        }
+    }
+}
