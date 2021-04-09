7d293ab5816531487de1bc4a54975d5d1cc77a24
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-04-09 11:32:56 +0300

    Lisätty List-luokan esimerkkejä.

--- DotNet/GeneerisetTietotyypit/GeneerisetTietotyypit/Program.cs
+++ DotNet/GeneerisetTietotyypit/GeneerisetTietotyypit/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GeneerisetTietotyypit
 {
@@ -7,6 +8,45 @@ namespace GeneerisetTietotyypit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            // merkkijonot
+            List<string> merkkijonot = new();
+            merkkijonot.Add("Testi");
+            merkkijonot.Add("Toinen");
+            merkkijonot.Add("Kolmas");
+            // merkkijonot.Add(1234);
+            // merkkijonot.Add(true);
+
+            merkkijonot.Remove("Toinen");
+            merkkijonot.RemoveAt(1);
+
+            foreach (string merkkijono in merkkijonot)
+            {
+                Console.WriteLine(merkkijono);
+            }
+
+            // autot
+            Auto a1 = new()
+            {
+                Merkki = "Toyota",
+                Malli = "Corolla",
+                Huippunopeus = 190
+            };
+            Auto a2 = new()
+            {
+                Merkki = "Audi",
+                Malli = "A4",
+                Huippunopeus = 230
+            };
+
+            List<Auto> autot = new();
+            autot.Add(a1);
+            autot.Add(a2);
+
+            foreach (Auto auto in autot)
+            {
+                Console.WriteLine(auto.Merkki);
+            }
         }
     }
 }
