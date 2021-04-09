8358ced85df0b435c6a45ac0380af254c6629577
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-04-09 15:26:09 +0300

    Lisätty try-catch -lauseet koodiin.

--- DotNet/Päivämäärät/Päivämäärät/Program.cs
+++ DotNet/Päivämäärät/Päivämäärät/Program.cs
@@ -13,22 +13,30 @@ namespace Päivämäärät
             // tehtävä 2
             Console.WriteLine("Anna päivämäärä:");
             string pvm = Console.ReadLine();
-            DateTime päivä = DateTime.Parse(pvm);
 
-            const int PäiviäViikossa = 7;
-            DateTime viikonPäästä = päivä.AddDays(PäiviäViikossa);
+            try
+            {
+                DateTime päivä = DateTime.Parse(pvm);
 
-            Console.WriteLine("Annettu päivä:");
-            Console.WriteLine(päivä);
-            Console.WriteLine(päivä.Year);
-            Console.WriteLine(päivä.Month);
-            Console.WriteLine(päivä.Day);
-            
-            Console.WriteLine("Viikon päästä:");
-            Console.WriteLine(viikonPäästä);
-            Console.WriteLine(viikonPäästä.Year);
-            Console.WriteLine(viikonPäästä.Month);
-            Console.WriteLine(viikonPäästä.Day);
+                const int PäiviäViikossa = 7;
+                DateTime viikonPäästä = päivä.AddDays(PäiviäViikossa);
+
+                Console.WriteLine("Annettu päivä:");
+                Console.WriteLine(päivä);
+                Console.WriteLine(päivä.Year);
+                Console.WriteLine(päivä.Month);
+                Console.WriteLine(päivä.Day);
+
+                Console.WriteLine("Viikon päästä:");
+                Console.WriteLine(viikonPäästä);
+                Console.WriteLine(viikonPäästä.Year);
+                Console.WriteLine(viikonPäästä.Month);
+                Console.WriteLine(viikonPäästä.Day);
+            }
+            catch
+            {
+                Console.WriteLine("Virheellinen päivämäärä.");
+            }
         }
     }
 }
