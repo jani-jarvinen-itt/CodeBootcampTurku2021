3bf0cee1e1cbdd43ee14e2227becd81990a8bfec
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-04-09 15:41:10 +0300

    Lisätty kulttuuri-esimerkki.

---
+++ DotNet/Kulttuurit/Kulttuurit/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Kulttuurit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime nyt = DateTime.Now;
+            Console.WriteLine(nyt);
+
+            CultureInfo fi = new("fi");
+            CultureInfo enUs = new("en-US");
+            CultureInfo enGb = new("en-GB");
+            CultureInfo de = new("de");
+
+            Console.WriteLine(nyt.ToString(fi));
+            Console.WriteLine(nyt.ToString(enUs));
+            Console.WriteLine(nyt.ToString(enGb));
+            Console.WriteLine(nyt.ToString(de));
+
+            Console.WriteLine(nyt.ToString("dddd", fi));
+            Console.WriteLine(nyt.ToString("dddd", enUs));
+            Console.WriteLine(nyt.ToString("dddd", de));
+        }
+    }
+}
