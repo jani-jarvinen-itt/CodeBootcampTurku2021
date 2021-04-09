9bf118e1d2f4446be1edf826d6b4ccf1ebb945d8
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-04-09 11:00:43 +0300

    Aloitettu geneeristen tietotyyppien esimerkkisovelluksen koodausta.

---
+++ DotNet/GeneerisetTietotyypit/GeneerisetTietotyypit/Auto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneerisetTietotyypit
+{
+    public class Auto
+    {
+        public int Huippunopeus { get; set; }
+
+        public string Merkki { get; set; }
+
+        public string Malli { get; set; }
+    }
+}
---
+++ DotNet/GeneerisetTietotyypit/GeneerisetTietotyypit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GeneerisetTietotyypit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
