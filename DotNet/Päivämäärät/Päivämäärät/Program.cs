8f1bb739819588e8ab6efbdf76eb58e8ef269d54
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-04-09 15:00:44 +0300

    Aloitettu päivämäärien käsittelyesimerkkiä.

---
+++ DotNet/Päivämäärät/Päivämäärät/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Päivämäärät
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // tehtävä 1
+            DateTime syntymäaika = new DateTime(2000, 1, 15);
+            Console.WriteLine(syntymäaika.DayOfWeek);
+
+            // tehtävä 2
+            Console.WriteLine("Anna päivämäärä:");
+            string pvm = Console.ReadLine();
+            DateTime päivä = DateTime.Parse(pvm);
+
+            const int PäiviäViikossa = 7;
+            DateTime viikonPäästä = päivä.AddDays(PäiviäViikossa);
+
+            Console.WriteLine("Annettu päivä:");
+            Console.WriteLine(päivä);
+            Console.WriteLine(päivä.Year);
+            Console.WriteLine(päivä.Month);
+            Console.WriteLine(päivä.Day);
+            
+            Console.WriteLine("Viikon päästä:");
+            Console.WriteLine(viikonPäästä);
+            Console.WriteLine(viikonPäästä.Year);
+            Console.WriteLine(viikonPäästä.Month);
+            Console.WriteLine(viikonPäästä.Day);
+        }
+    }
+}
