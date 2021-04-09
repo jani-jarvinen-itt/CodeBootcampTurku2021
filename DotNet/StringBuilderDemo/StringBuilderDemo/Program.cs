08da0fcac84475d68072fa7f0dfbab39f0b53327
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-04-09 10:34:52 +0300

    Lisätty StringBuilder-esimerkkiin suorituskykyisempi tapa merkkijonojen muodostamiseen. Suoritusaika nyt n. 0,05 sekuntia.

--- DotNet/StringBuilderDemo/StringBuilderDemo/Program.cs
+++ DotNet/StringBuilderDemo/StringBuilderDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace StringBuilderDemo
 {
@@ -8,6 +9,8 @@ namespace StringBuilderDemo
         {
             Console.WriteLine("Hello World!");
 
+            /*
+            // suoritusaika n. 90 sekuntia
             DateTime alku = DateTime.Now;
             string html = "<table>";
             for (int i = 0; i < 100000; i++)
@@ -17,9 +20,27 @@ namespace StringBuilderDemo
             html += "</table>";
             Console.WriteLine("HTML-koodin pituus: " + html.Length);
             DateTime loppu = DateTime.Now;
+            */
+
+            DateTime alku = DateTime.Now;
+            StringBuilder html = new("<table>");
+            for (int i = 0; i < 100000; i++)
+            {
+                html.AppendLine("<tr><td>" + i + "</td></tr>");
+            }
+            html.AppendLine("</table>");
+            string valmisHtml = html.ToString();
+            Console.WriteLine("HTML-koodin pituus: " + valmisHtml.Length);
+            DateTime loppu = DateTime.Now;
 
             TimeSpan kesto = loppu - alku;
             Console.WriteLine($"HTML-koodin muodostus kesti: {kesto.TotalSeconds}.");
+
+
+            string etunimi = "Teppo";
+            string sukunimi = "Testaaja";
+            string kokoNimi = etunimi + " " + sukunimi;
+
         }
     }
 }
