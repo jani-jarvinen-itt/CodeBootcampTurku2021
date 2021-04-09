44f528551ea1f819f3e679a552d46b3f9915f174
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-04-09 13:42:31 +0300

    Aloitettu sanakirja-esimerkin koodaus.

---
+++ DotNet/Sanakirjat/Sanakirjat/Pelihahmo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanakirjat
+{
+    public class Pelihahmo
+    {
+        public int Pistemäärä { get; set; }
+
+        public int Elinvoima { get; set; }
+
+        public int AmmustenMäärä { get; set; }
+    }
+}
---
+++ DotNet/Sanakirjat/Sanakirjat/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sanakirjat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Dictionary<int, string> asiakkaat = new();
+            asiakkaat.Add(100, "Teppo Testaaja");
+            asiakkaat.Add(101, "Antti Asiakas");
+            asiakkaat.Remove(100);
+
+            // tapa 1
+            foreach (var /*KeyValuePair<int, string>*/ asiakas in asiakkaat)
+            {
+                Console.WriteLine(asiakas.Key + ": " + asiakas.Value);
+            }
+
+            // tapa 2
+            foreach (int avain in asiakkaat.Keys)
+            {
+                string nimi = asiakkaat[avain];
+                Console.WriteLine(avain + ": " + nimi);
+            }
+
+
+
+
+
+
+
+            /*Dictionary<int, Pelihahmo> pelihahmot = new();*/
+        }
+    }
+}
