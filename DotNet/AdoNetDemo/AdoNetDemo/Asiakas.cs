4e02a6a12c00cec96c6c92774158a65821d9ccdf
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-04-13 13:26:57 +0300

    Lisätty alustava versio asiakastietojen kysymisestä käyttäjältä.

---
+++ DotNet/AdoNetDemo/AdoNetDemo/Asiakas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetDemo
+{
+    public class Asiakas
+    {
+        public string Avain { get; set; }
+
+        public string YrityksenNimi { get; set; }
+
+        public string Kontaktihenkilö { get; set; }
+
+        public string Titteli { get; set; }
+    }
+}
--- DotNet/AdoNetDemo/AdoNetDemo/Program.cs
+++ DotNet/AdoNetDemo/AdoNetDemo/Program.cs
@@ -30,14 +30,39 @@ namespace AdoNetDemo
 
             // INSERT-esimerkki
             Console.WriteLine("Aloitetaan uuden asiakkaan lisääminen...");
+            Asiakas asiakas = LueTiedot();
             sql = "INSERT INTO [dbo].[Customers] ([CustomerID],[CompanyName],[ContactName],[ContactTitle],[Address], "+
                   "[City],[Region],[PostalCode],[Country],[Phone],[Fax]) "+
-                  "VALUES ('UUSIA', 'Uusi Yritys Oy', 'Teppo Testaaja', "+
-                  "'Pääjehu', 'Teollisuuskatu 17', 'Forssa', "+
+                  $"VALUES ('{asiakas.Avain}', '{asiakas.YrityksenNimi}', "+
+                  $"'{asiakas.Kontaktihenkilö}', '{asiakas.Titteli}', "+
+                  "'Teollisuuskatu 17', 'Forssa', "+
                   "NULL, '21450', 'Finland', '0400 98765', NULL)";
             SqlCommand komento2 = new(sql, yhteys);
             int rivienMäärä = komento2.ExecuteNonQuery();
             Console.WriteLine($"Lisätty {rivienMäärä} uusi asiakas tietokantaan.");
+        }
+
+        private static Asiakas LueTiedot()
+        {
+            Asiakas asiakas = new();
+
+            Console.WriteLine("Anna asiakkaan avainkoodi (5 merkkiä):");
+            string syöte = Console.ReadLine();
+            asiakas.Avain = syöte;
+
+            Console.WriteLine("Anna asiakkaan nimi:");
+            syöte = Console.ReadLine();
+            asiakas.YrityksenNimi = syöte;
+
+            Console.WriteLine("Anna asiakkaan kontaktihenkilö:");
+            syöte = Console.ReadLine();
+            asiakas.Kontaktihenkilö = syöte;
+
+            Console.WriteLine("Anna asiakkaan titteli:");
+            syöte = Console.ReadLine();
+            asiakas.Titteli = syöte;
+
+            return asiakas;
         }
     }
 }
