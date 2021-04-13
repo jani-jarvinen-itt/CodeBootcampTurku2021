58bf4dd0e98d6162726bedde746a92a888def5c0
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-04-13 10:48:34 +0300

    Lisätty ADO.NET-esimerkkikoodi.

--- DotNet/AdoNetDemo/AdoNetDemo/Program.cs
+++ DotNet/AdoNetDemo/AdoNetDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 
 namespace AdoNetDemo
 {
@@ -7,6 +8,24 @@ namespace AdoNetDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            // tietokantayhteyden avaus
+            string yhteysmerkkijono = "Server=localhost\\SQLEXPRESS;Database=Northwind;Trusted_Connection=True;";
+            SqlConnection yhteys = new(yhteysmerkkijono);
+            yhteys.Open();
+            Console.WriteLine("Tietokantayhteys avattu!");
+
+            // SQL-kyselyn ajaminen
+            string sql = "SELECT * FROM Customers WHERE Country = 'Finland'";
+            SqlCommand komento = new(sql, yhteys);
+            SqlDataReader lukija = komento.ExecuteReader();
+
+            // tietojen lukeminen
+            while (lukija.Read())
+            {
+                string yritysNimi = lukija["CompanyName"].ToString();
+                Console.WriteLine(yritysNimi);
+            }
         }
     }
 }
