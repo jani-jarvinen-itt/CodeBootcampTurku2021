035fe457aee486f46f034c339aefedb92031caad
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-04-14 11:44:37 +0300

    Lisätty API-kontrolleriin kaksi metodia.

--- DotNet/AspNetWebApiDemo/AspNetWebApiDemo/Controllers/TestiApiController.cs
+++ DotNet/AspNetWebApiDemo/AspNetWebApiDemo/Controllers/TestiApiController.cs
@@ -11,11 +11,18 @@ namespace AspNetWebApiDemo.Controllers
     [ApiController]
     public class TestiApiController : ControllerBase
     {
+        [Route("satunnainen")]
         public int Luku()
         {
             Random satunnainen = new();
             int luku = satunnainen.Next(1, 10000);
             return luku;
+        }
+
+        [Route("kellonaika")]
+        public DateTime Kellonaika()
+        {
+            return DateTime.Now;
         }
     }
 }
