3f19cabc714a56fbdc3d06374b686d73fc66f636
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-04-14 11:30:39 +0300

    Aloitettu tekemään ASP.NET Web API -sovellusta ja uuden API-kontrollerin lisäämistä.

--- DotNet/EntityFrameworkDemo/EntityFrameworkDemo/Program.cs
+++ DotNet/EntityFrameworkDemo/EntityFrameworkDemo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using EntityFrameworkDemo.Models;
 using System;
+using System.Linq;
 
 namespace EntityFrameworkDemo
 {
@@ -10,10 +11,33 @@ namespace EntityFrameworkDemo
             Console.WriteLine("Hello World!");
 
             NorthwindContext konteksti = new();
-            foreach (var asiakas in konteksti.Customers)
+            // var quedenTilaukset = konteksti.Orders.Where(o => o.CustomerId == "QUEDE");
+
+            // LINQ-kysely
+            var suomalaiset = konteksti.Customers.Where(c => c.Country == "Finland");
+            foreach (var asiakas in suomalaiset)
             {
                 Console.WriteLine(asiakas.CompanyName);
             }
+
+            /*
+            // toimii, mutta suorituskyvyn kannalta heikko
+            NorthwindContext konteksti = new();
+            foreach (var asiakas in konteksti.Customers)
+            {
+                if (asiakas.Country == "Finland")
+                {
+                    Console.WriteLine(asiakas.CompanyName);
+                }
+            }
+            */
         }
+
+        /*
+        private bool Testi(Customer c)
+        {
+            return c.Country == "Finland";
+        }
+        */
     }
 }
