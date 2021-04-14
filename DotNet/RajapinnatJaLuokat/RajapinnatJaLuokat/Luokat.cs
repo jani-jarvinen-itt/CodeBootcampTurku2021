7cb7a29b47c183c4adf3325571d781298ac0e23a
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-04-14 14:41:11 +0300

    Lisätty C#-rajapinta- ja luokkaesimerkki.

---
+++ DotNet/RajapinnatJaLuokat/RajapinnatJaLuokat/Luokat.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RajapinnatJaLuokat
+{
+    public class Auto : ITulostus
+    {
+        public string Merkki { get; set; }
+        public string Malli { get; set; }
+
+        public void Tulosta()
+        {
+            Console.WriteLine($"Auto: merkki={Merkki}, malli={Malli}.");
+        }
+    }
+
+    public class Koira : ITulostus
+    {
+        public string Nimi { get; set; }
+        public string Rotu { get; set; }
+        public int Ikä { get; set; }
+
+        public void Tulosta()
+        {
+            Console.WriteLine($"Koira: nimi={Nimi}, rotu={Rotu}, ikä={Ikä}.");
+        }
+    }
+
+    public interface ITulostus
+    {
+        void Tulosta();
+    }
+}
---
+++ DotNet/RajapinnatJaLuokat/RajapinnatJaLuokat/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RajapinnatJaLuokat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Auto a = new()
+            {
+                Merkki = "Ford",
+                Malli = "Fiesta"
+            };
+            Koira k = new()
+            {
+                Nimi = "Musti",
+                Rotu = "Collie",
+                Ikä = 7
+            };
+
+            ITulostus tulostus = k;
+            Raportoi(a);
+            Raportoi(tulostus);
+        }
+
+        static void Raportoi(ITulostus t)
+        {
+            t.Tulosta();
+        }
+    }
+}
