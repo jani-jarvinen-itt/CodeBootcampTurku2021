f2f10965b4bd81021ea9cf5680607b9a0ce1968f
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-04-19 10:51:41 +0300

    Lisätty Reactin komponentti luokkamäärityksillä.

---
+++ React/my-app/src/LuokkaKomponentti.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class LuokkaKomponentti extends React.Component {
+    render() {
+        return <h1>Moi, tämä on LuokkaKomponentti!</h1>;
+    }
+}
+
+export default LuokkaKomponentti;
--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import PieniKomponentti from './PieniKomponentti';
+import LuokkaKomponentti from './LuokkaKomponentti';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
     <PieniKomponentti />
+    <LuokkaKomponentti />
   </React.StrictMode>,
   document.getElementById('root')
 );
