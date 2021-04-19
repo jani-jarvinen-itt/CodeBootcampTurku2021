4078bca6edf8d7c5b3e4aaddd40b4dc1170473dd
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-04-19 15:57:30 +0300

    Lisätty TypeScript-esimerkki.

---
+++ TypeScript/Muuttujat.js
@@ -0,0 +1,11 @@
+var luku = 123;
+console.log(luku);
+var nimi = "ABC";
+console.log(nimi);
+var tila = true;
+console.log(tila);
+function testi(luku) {
+    var plusYksi = luku + 1;
+    console.log(plusYksi);
+}
+testi(99);
---
+++ TypeScript/Muuttujat.ts
@@ -0,0 +1,15 @@
+let luku: number = 123;
+console.log(luku);
+
+let nimi: string = "ABC";
+console.log(nimi);
+
+let tila: boolean = true;
+console.log(tila);
+
+function testi(luku: number) {
+    let plusYksi = luku + 1;
+    console.log(plusYksi);
+}
+
+testi(99);
